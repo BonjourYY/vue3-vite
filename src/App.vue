a479d4c501fe6c1f006a281e892ed8039b05051b
范勤阳 <fqymtu@gmail.com>
2023-06-13 15:26:38 +0800

    add v23

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
-  <RouterLink :to="{ path: '/name' }">姓名</RouterLink>
+  <RouterLink :to="{ path: '/name' }">姓名v23</RouterLink>
   <br />
-  <RouterLink :to="{ path: '/hobby' }">爱好</RouterLink>
+  <RouterLink :to="{ path: '/hobby' }">爱好v23</RouterLink>
   <Suspense>
     <RouterView></RouterView>
   </Suspense>
